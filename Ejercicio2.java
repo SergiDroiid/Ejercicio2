8159559bca85654a624ea9d6985b86378e43a057
SergiDroiid <55207275+SergiDroiid@users.noreply.github.com>
2025-12-03 10:59:35 +0100

    Añadida variable de significado

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,5 +1,7 @@
 public class Ejercicio2 {
 public static void main(String[] args) {
+int numero = 42;
+System.out.println("El significado de la vida es: " + numero);
 System.out.println("Clonado con éxito!");
 }
 }
