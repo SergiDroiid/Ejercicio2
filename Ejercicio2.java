c194f0e8f75470f0363c219a984b0e5ada9127fd
SergiDroiid <55207275+SergiDroiid@users.noreply.github.com>
2025-12-03 17:10:27 +0100

    Pull Request

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
 int numero = 42;
 System.out.println("El significado de la vida es: " + numero);
 System.out.println("Clonado con éxito!");
+System.out.println("Hola Mundo");
 }
 }
